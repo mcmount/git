21ff3438aac58323e3d46634d09456583a7f47a1
mcmount <77773871+mcmount@users.noreply.github.com>
2021-01-21 10:56:35 +0300

    Update g1_p.py
    print('Again')

--- g1_p.py
+++ g1_p.py
@@ -2,7 +2,7 @@ def main():
     print('My first git program')
     print('And I change it every day')
     print('My first git program1')
-
+    print('Again')
 
 if __name__ == '__main__':
     main()
